0882695e6a1c77ee333ec14f520fed4442325c3a
Rich Wareham <rjw57@cam.ac.uk>
2017-04-10 11:22:57 +0100

    draw bordered window on screen

--- red/__init__.py
+++ red/__init__.py
@@ -1,4 +1,5 @@
 import curses
+import enum
 
 def main():
     editor = Editor()
@@ -13,8 +14,13 @@ def ctrl_key(key):
 
 class Editor:
     def __init__(self):
-        self.screen = False
+        self.screen = None
         self.should_exit = False
+
+        # A simple dictionary mapping key-presses to callables.
+        self.key_bindings = {
+            ctrl_key('q'): self.quit,
+        }
 
     def curses_main(self, screen):
         """Wrapped function after terminal is set up and the alternate screen
@@ -27,10 +33,73 @@ class Editor:
         # Record the current curses screen
         self.screen = screen
 
+        # Set up the colour pairs
+        setup_curses_colour_pairs()
+
+        # Paint the initial screen
+        self.redraw()
+
         # Start event loop
         while not self.should_exit:
-            self.handle_key(screen.get_wch())
+            # Get next keypress
+            ch = screen.get_wch()
 
-    def handle_key(self, key):
-        if key == ctrl_key('q'):
-            self.should_exit = True
+            # Look up keypress in key bindings dict
+            handler = self.key_bindings.get(ch)
+            if handler is not None:
+                handler()
+
+            self.redraw()
+
+    def redraw(self):
+        """Redraw the screen."""
+        s = self.screen
+
+        s.bkgdset(' ', curses.color_pair(ColourPairs.WINDOW_BACKGROUND))
+        s.erase()
+
+        s.attrset(curses.color_pair(ColourPairs.WINDOW_BORDER))
+        s.border()
+
+        s.refresh()
+
+    def quit(self):
+        """Signal that the application should exit."""
+        self.should_exit = True
+
+def setup_curses_colour_pairs():
+    """Associate sensible colour pairs for the values in ColourPairs."""
+    if curses.COLORS == 256:
+        p = TerminalPalette256
+    else:
+        raise RuntimeError('Only 256 colour terminals supported')
+
+    curses.init_pair(ColourPairs.WINDOW_BORDER, p.BRIGHT_WHITE, p.BLUE)
+    curses.init_pair(ColourPairs.WINDOW_BACKGROUND, p.LIGHT_GREY, p.BLUE)
+
+class ColourPairs(enum.IntEnum):
+    """Colour pair numbers."""
+
+    WINDOW_BORDER = 1
+    WINDOW_BACKGROUND = 2
+
+class TerminalPalette256(enum.IntEnum):
+    """A terminal palette suitable for 256 colour displays."""
+
+    BLACK = 16
+    BLUE = 19
+    GREEN = 34
+    CYAN = 37
+    RED = 124
+    MAGENTA = 127
+    BROWN = 130
+    LIGHT_GREY = 248
+    DARK_GREY = 240
+
+    BRIGHT_BLUE = 63
+    BRIGHT_GREEN = 83
+    BRIGHT_CYAN = 87
+    BRIGHT_RED = 203
+    BRIGHT_MAGENTA = 207
+    BRIGHT_YELLOW = 227
+    BRIGHT_WHITE = 231
